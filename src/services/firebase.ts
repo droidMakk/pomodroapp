3bac0481ec9357315e7cbae3450179267958474d
Afroze Kabeer Khan <droidmakk@gmail.com>
2019-04-17 12:48:49 +0530

    Removed config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 let config = {
-    apiKey: "AIzaSyBd0rXnoOueqO2yHZ3WjN-gPW_3d7nuYRY",
-    authDomain: "pomdroapp.firebaseapp.com",
-    databaseURL: "https://pomdroapp.firebaseio.com",
-    projectId: "pomdroapp",
-    storageBucket: "pomdroapp.appspot.com",
-    messagingSenderId: "903821659307"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
